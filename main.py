40f574bf26fde83236a67eccce6dffaf4d58624b
Abhinav Silwal <abhinavsilwal93@gmail.com>
2026-06-28 10:52:36 -0400

    Add project directory scanning

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import typer
+from pathlib import Path
 
 
 app = typer.Typer()
@@ -14,6 +15,31 @@ def hello():
 def version():
     # Show version of the tool.
     print("v0.1.0")
+
+
+@app.command()
+def scan(path: str = typer.Argument(".")):
+    # Scan a project directory and list Python files
+    project_path = Path(path)
+
+    print(f"Scanning project: {project_path.resolve()}\n")
+
+    if not project_path.exists():
+        print("Path does not exist.")
+        raise typer.Exit()
+
+    python_files = []
+
+    for file in project_path.rglob("*.py"):
+        if ".venv" in file.parts or "__pycache__" in file.parts:
+            continue
+
+        python_files.append(file)
+
+    print(f"Found {len(python_files)} Python files:\n")
+
+    for file in python_files:
+        print(f"- {file.relative_to(project_path)}")
 
 
 if __name__ == "__main__":
